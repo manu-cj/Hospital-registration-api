a877e89936e90d65e427668d416a0f653eedfc35
manu-cj <carenjotje@gmail.com>
2025-06-04 11:00:37 +0200

    init

--- src/main/java/org/manu/controller/HomeController.java
+++ src/main/java/org/manu/controller/HomeController.java
@@ -2,17 +2,14 @@ package org.manu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+
 
 @RestController
 public class HomeController {
 
   @GetMapping("/")
-  public String home(@AuthenticationPrincipal OAuth2User principal) {
-    if (principal != null) {
-      return "Bienvenue, " + principal.getAttribute("name") + " !";
-    }
-    return "Bienvenue sur l'application Spring Boot sécurisée avec OAuth2 !";
+  public String home() {
+
+    return "Bienvenue sur l'application Spring Boot !";
   }
 }
